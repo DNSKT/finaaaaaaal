c2e33ef05462a4d5c805ce0ff34dc128c9c92342
Skult_ <33635788+DNSKT@users.noreply.github.com>
2018-12-06 18:24:22 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -247,4 +247,4 @@ client.on("ready", () => {
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
-client.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
